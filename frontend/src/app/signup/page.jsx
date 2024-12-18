7f5ea6234b1e0ee42397428ff838202bacdb44ac
HENRY MUTEGA <50877662+henrymutega@users.noreply.github.com>
2024-12-18 16:40:02 +0300

    signup page.

--- frontend/src/app/signup/page.jsx
+++ frontend/src/app/signup/page.jsx
@@ -2,7 +2,33 @@ import React from 'react'
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <div className="wrapper">
+    <h2>Registration</h2>
+    <form action="#">
+      <div className="input-box">
+        <input type="text" placeholder="Enter your name" required/>
+      </div>
+      <div className="input-box">
+        <input type="text" placeholder="Enter your email" required/>
+      </div>
+      <div className="input-box">
+        <input type="password" placeholder="Create password" required/>
+      </div>
+      <div className="input-box">
+        <input type="password" placeholder="Confirm password" required/>
+      </div>
+      <div className="policy">
+        <input type="checkbox"/>
+        <h3>I accept all terms & condition</h3>
+      </div>
+      <div className="input-box button">
+        <input type="Submit" value="Register Now"/>
+      </div>
+      <div className="text">
+        <h3>Already have an account? <a href="/login">Login now</a></h3>
+      </div>
+    </form>
+  </div>
   )
 }
 
